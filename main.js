33c1db9592875c869ea828380a824981aa7ab881
Артем Бажин <bazhin@selectel.ru>
2018-03-01 21:28:39 +0300

    Initial commit in callback branch

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ const path = require('path');
 
 const getArgs = require('./getArgs');
 const getFileList = require('./getFileList');
-const writeFile = require('./writeFile');
+//const writeFile = require('./writeFile');
 
 const args = getArgs();
-const files = getFileList(args.src);
+//const files = getFileList(args.src);
 
 //console.log(path.parse('~/test'));
 
-files.forEach(str => writeFile(str, args.dest));
+//files.forEach(str => writeFile(str, args.dest));
+
+getFileList(args, files => {
+  console.log(files);
+});
